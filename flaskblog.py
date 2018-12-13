53e7361cc37a576b2284809510929ee4d813664a
shubi25 <shuaibugirei5@gmail.com>
2018-12-13 14:58:22 +0100

    Dashboard has been added to the app.

--- flaskblog.py
+++ flaskblog.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, url_for
 from flask_mysqldb import MySQL
 
 app = Flask(__name__)
@@ -11,23 +11,34 @@ app.config['MYSQL_DB'] = 'fuel_system'
 
 mysql = MySQL(app)
 
-@app.route("/")
-def hello():
-    return render_template('home.html')
+
+@app.route("/index")
+def index():
+    return render_template('index.html')
 
 
-@app.route("/", methods=['POST'])
-def Authenticate():
+@app.route("/table")
+def table():
+	cursor = mysql.connection.cursor()
+	result = cursor.execute("SELECT * from driver")
+	if result > 0:
+		userDetails = cursor.fetchall()
+	return render_template('tables.html',userDetails=userDetails)
 
-    username = request.form['u']
-    password = request.form['p']
-    cursor = mysql.connection.cursor()
-    cursor.execute("SELECT * from Users where username='" + username +"' and password='" + password +"'")
-    data = cursor.fetchone()
-    if data is None:
-    	return "Username or Password is Incorrect"
-    else:
-    	return "Logged In successfully"
+
+@app.route("/", methods=['GET','POST'])
+def home():
+	if request.method == 'POST':
+	    username = request.form['u']
+	    password = request.form['p']
+	    cursor = mysql.connection.cursor()
+	    cursor.execute("SELECT * from Users where username='" + username +"' and password='" + password +"'")
+	    data = cursor.fetchone()
+	    if data is None:
+	    	return "Username or Password is Incorrect"
+	    else:
+	    	return redirect(url_for('index'))
+	return render_template('home.html')
 
 
 @app.route("/driver", methods=['GET','POST'])
@@ -62,7 +73,20 @@ def vehicle():
    return render_template('vehicle.html')
     
 
-
+@app.route("/request", methods=['GET','POST'])
+def fuelrequest():
+   if request.method == 'POST':
+   	   # fetch form data
+	   userDetails = request.form
+	   name = userDetails['name']
+	   plateNum = userDetails['plateNum']
+	   fuelRequest = userDetails['requested_fuel']
+	   cursor = mysql.connection.cursor()
+	   cursor.execute("INSERT INTO fuel_request(driver_name, vehicle_plate_no, requested_fuel) VALUES(%s, %s, %s)",(name, plateNum, fuelRequest))
+	   cursor.connection.commit()
+	   cursor.close
+	   return 'Success'
+   return render_template('request.html')
 
 if __name__ == '__main__':
   	app.run(debug=True)
